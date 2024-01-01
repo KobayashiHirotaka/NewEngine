7f5bf58066f9c8fff2cb345620d91d015862aef4
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2024-01-01 22:59:41 +0900

    Weapon属性の追加

--- Engine/Utility/Collision/CollisionConfig.h
+++ Engine/Utility/Collision/CollisionConfig.h
@@ -3,9 +3,11 @@
 
 const uint32_t kCollisionAttributePlayer = 0b0001;
 const uint32_t kCollisionAttributeEnemy = 0b0010;
+const uint32_t kCollisionAttributeWeapon = 0b0100;
 
 const uint32_t kCollisionMaskPlayer = 0b1110;
 const uint32_t kCollisionMaskEnemy = 0b1101;
+const uint32_t kCollisionMaskWeapon = 0b1011;
 
 const uint32_t kCollisionPrimitiveSphere = 0b1;
 const uint32_t kCollisionPrimitiveAABB = 0b1 << 1;
