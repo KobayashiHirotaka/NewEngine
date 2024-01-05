0b0e0031fad14ba3f618c3460e042ba37dca5817
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2024-01-06 01:56:44 +0900

    投げ完成

--- Project/GameObject/Character/Enemy/Enemy.cpp
+++ Project/GameObject/Character/Enemy/Enemy.cpp
@@ -97,7 +97,7 @@ void Enemy::OnCollision(Collider* collider, float damage)
 
 		if (player_->GetIsThrow() == true && isDown_ == false)
 		{
-			damage = 3.0f;
+			damage = 5.0f;
 			HP_ -= damage;
 			isHitThrow_ = true;
 		}
