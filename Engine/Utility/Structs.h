f3e5ac58cbf86af2209cc6fc85b8835dfae50b65
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-04-18 12:55:43 +0900

    KeyframeAnimationの構造体を追加

--- Engine/Utility/Structs.h
+++ Engine/Utility/Structs.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <map>
 
 //structs
 struct Vector2
@@ -27,6 +32,12 @@ struct Matrix4x4
 	float m[4][4];
 };
 
+struct Quaternion 
+{
+	float x, y, z, w;
+};
+
+
 struct AABB
 {
 	Vector3 min;
@@ -39,4 +50,39 @@ struct VertexData
 	Vector4 position;
 	Vector2 texcoord;
 	Vector3 normal;
+};
+
+//Keyframe構造体
+template <typename tValue>
+
+struct Keyframe 
+{
+	float time;//キーフレームの時間(単位：秒)
+	tValue value;//キーフレームの値
+};
+
+using KeyframeVector3 = Keyframe<Vector3>;
+using KeyframeQuaternion = Keyframe<Quaternion>;
+
+
+//KeyframeをNodeごとにまとめる
+template <typename tValue>
+struct AnimationCurve 
+{
+	std::vector<Keyframe<tValue>> keyframes;
+};
+
+struct NodeAnimation
+{
+	AnimationCurve<Vector3> translate;
+	AnimationCurve<Quaternion> rotate;
+	AnimationCurve<Vector3> scale;
+};
+
+struct Animation 
+{
+	float duration;//アニメーションの全体の長さ(単位：秒)
+
+	//NodeAnimationの集合、Node名で引けるようにする
+	std::map<std::string, NodeAnimation> nodeAnimations;
 };
