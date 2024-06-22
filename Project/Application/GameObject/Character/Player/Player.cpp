f567f9bc6bc93a68a14900e221c097b284528fce
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-06-23 00:09:37 +0900

    Playerの基本移動の移行完了

--- Project/Application/GameObject/Character/Player/Player.cpp
+++ Project/Application/GameObject/Character/Player/Player.cpp
@@ -1,5 +1,10 @@
 #include "Player.h"
 
+Player::~Player()
+{
+	
+}
+
 void Player::Initialize()
 {
 	IGame3dObject::SetTag("Player");
@@ -9,6 +14,9 @@ void Player::Initialize()
 
 	//inputのinstance
 	input_ = Input::GetInstance();
+
+	//audioのinstance
+	audio_ = Audio::GetInstance();
 
 	//worldTransformの初期化
 	worldTransform_.Initialize();
@@ -21,81 +29,68 @@ void Player::Initialize()
 
 void Player::Update()
 {
-	if (input_->PressKey(DIK_0))
-	{
-		animationIndex = 0;
-		float animationTime;
-		animationTime = model_->GetAnimationTime();
-
-		animationTime += 1.0f / 60.0f;
-		animationTime = std::fmod(animationTime, model_->GetAnimation()[0].duration);
-
-		model_->SetAnimationTime(animationTime);
-
-		model_->ApplyAnimation(0);
-
-		model_->Update();
-	}
-
-	if (input_->PressKey(DIK_1))
-	{
-		animationIndex = 1;
-		float animationTime;
-		animationTime = model_->GetAnimationTime();
-
-		animationTime += 1.0f / 60.0f;
-		animationTime = std::fmod(animationTime, model_->GetAnimation()[1].duration);
-
-		model_->SetAnimationTime(animationTime);
-
-		model_->ApplyAnimation(1);
-
-		model_->Update();
-	}
-
-	if (input_->PressKey(DIK_2))
-	{
-		animationIndex = 2;
-		float animationTime;
-		animationTime = model_->GetAnimationTime();
-
-		animationTime += 1.0f / 60.0f;
-		animationTime = std::fmod(animationTime, model_->GetAnimation()[2].duration);
-
-		model_->SetAnimationTime(animationTime);
-
-		model_->ApplyAnimation(2);
-
-		model_->Update();
-	}
-
+	//0は停止、1は攻撃(振り下ろし),2は歩き
 	model_->ApplyAnimation(animationIndex);
 
 	model_->Update();
 
-	//パーティクルの更新
-	particleSystem_->Update();
-
-	if (input_->PushKey(DIK_P))
+	//PlayerのBehavior
+	if (behaviorRequest_)
 	{
-		ParticleEmitter* newParticleEmitter = EmitterBuilder()
-			.SetParticleType(ParticleEmitter::ParticleType::kNormal)
-			.SetTranslation(worldTransform_.translation)
-			.SetArea({ 0.0f,0.0f,0.0f }, { 0.0f,0.0f,0.0f })
-			.SetRotation({ 0.0f,0.0f,0.0f }, { 0.0f,0.0f,0.0f })
-			.SetScale({ 0.2f, 0.2f,0.2f }, { 0.6f ,0.6f ,0.6f })
-			.SetAzimuth(0.0f, 360.0f)
-			.SetElevation(0.0f, 0.0f)
-			.SetVelocity({ 0.06f ,0.06f ,0.06f }, { 0.1f ,0.1f ,0.1f })
-			.SetColor({ 1.0f ,1.0f ,1.0f ,1.0f }, { 1.0f ,1.0f ,1.0f ,1.0f })
-			.SetLifeTime(0.1f, 1.0f)
-			.SetCount(100)
-			.SetFrequency(4.0f)
-			.SetDeleteTime(2.0f)
-			.Build();
-		particleSystem_->AddParticleEmitter(newParticleEmitter);
+		behavior_ = behaviorRequest_.value();
+
+		switch (behavior_)
+		{
+		case Behavior::kRoot:
+		default:
+			BehaviorRootInitialize();
+			break;
+
+		case Behavior::kAttack:
+			BehaviorAttackInitialize();
+			break;
+
+		case Behavior::kJump:
+			BehaviorJumpInitialize();
+			break;
+
+		case Behavior::kThrow:
+			BehaviorThrowInitialize();
+			break;
+
+		case Behavior::kStan:
+			BehaviorStanInitialize();
+			break;
+		}
+
+		behaviorRequest_ = std::nullopt;
 	}
 
+	switch (behavior_)
+	{
+	case Behavior::kRoot:
+	default:
+		BehaviorRootUpdate();
+		break;
+
+	case Behavior::kAttack:
+		BehaviorAttackUpdate();
+		break;
+
+	case Behavior::kJump:
+		BehaviorJumpUpdate();
+		break;
+
+	case Behavior::kThrow:
+		BehaviorThrowUpdate();
+		break;
+
+	case Behavior::kStan:
+		BehaviorStanUpdate();
+		break;
+	}
+
+	//imGui
 	model_->GetLight()->ImGui("DirectionalLight");
 	model_->GetPointLight()->ImGui("PointLight");
 	model_->GetSpotLight()->ImGui("SpotLight");
@@ -103,6 +98,7 @@ void Player::Update()
 	ImGui::Begin("Player");
 	ImGui::SliderFloat3("WTFT", &worldTransform_.translation.x, -100.0f, 100.0f);
 	ImGui::SliderFloat3("WTFR", &worldTransform_.rotation.x, 0.0f, 16.0f);
+	ImGui::Text("isGuard %d", isGuard_);
 	ImGui::End();
 
 	//worldTransformの更新
@@ -122,4 +118,241 @@ void Player::BoneDraw(const Camera& camera)
 void Player::DrawParticle(const Camera& camera)
 {
 	particleModel_->Draw(particleSystem_.get(), camera);
+}
+
+void Player::BehaviorRootInitialize()
+{
+
+}
+
+void Player::BehaviorRootUpdate()
+{
+	//コントローラーの移動処理
+	if (input_->GetJoystickState())
+	{
+		const float deadZone = 0.7f;
+		bool isFrontMove_ = false;
+		bool isBackMove_ = false;
+
+		float animationTime = 0.0f;
+
+		velocity_ = { 0.0f, 0.0f, 0.0f };
+
+		//振り向きの処理
+		if (worldTransform_.translation.x > 15.0f)
+		{
+			playerDirection = Direction::Left;
+			worldTransform_.rotation.y = 1.7f;
+		}
+		else if (worldTransform_.translation.x <= 15.0f)
+		{
+			playerDirection = Direction::Right;
+			worldTransform_.rotation.y = 4.6f;
+		}
+
+		/*Vector3 playerWorldPosition = GetWorldPosition();
+
+		Vector3 enemyWorldPosition = enemy_->GetWorldPosition();
+
+		if (enemyWorldPosition.x > playerWorldPosition.x)
+		{
+			worldTransform_.rotation.y = 1.7f;
+		}
+
+		if (enemyWorldPosition.x < playerWorldPosition.x)
+		{
+			worldTransform_.rotation.y = 4.6f;
+		}*/
+
+		//移動処理
+		//前方向に移動(左を向いているとき)
+		if (input_->IsPressButton(XINPUT_GAMEPAD_DPAD_LEFT) && playerDirection == Direction::Left && isDown_ == false && !isHit_)
+		{
+			velocity_.x = -0.3f;
+			isFrontMove_ = true;
+			isGuard_ = false;
+		}
+
+		//前方向に移動(右を向いているとき)
+		if (input_->IsPressButton(XINPUT_GAMEPAD_DPAD_RIGHT) && playerDirection == Direction::Right && isDown_ == false && !isHit_)
+		{
+			velocity_.x = 0.3f;
+			isFrontMove_ = true;
+			isGuard_ = false;
+		}
+
+		//後ろ方向に移動(右を向いているとき)
+		if (input_->IsPressButton(XINPUT_GAMEPAD_DPAD_LEFT) && playerDirection == Direction::Right && isDown_ == false)
+		{
+			isGuard_ = true;
+
+			//移動しながらガード
+			if (!input_->IsPressButton(XINPUT_GAMEPAD_DPAD_DOWN))
+			{
+				velocity_.x = -0.3f;
+				isBackMove_ = true;
+			}
+
+			//止まってガード
+			if (isGuard_ && input_->IsPressButton(XINPUT_GAMEPAD_DPAD_DOWN) && !input_->IsPressButton(XINPUT_GAMEPAD_DPAD_UP))
+			{
+				velocity_.x = 0.0f;
+				isBackMove_ = false;
+			}
+		}
+
+		//後ろ方向に移動(左を向いているとき)
+		if (input_->IsPressButton(XINPUT_GAMEPAD_DPAD_RIGHT) && playerDirection == Direction::Left && isDown_ == false)
+		{
+			isGuard_ = true;
+
+			//移動しながらガード
+			if (!input_->IsPressButton(XINPUT_GAMEPAD_DPAD_DOWN))
+			{
+				velocity_.x = 0.3f;
+				isBackMove_ = true;
+			}
+
+			//止まってガード
+			if (isGuard_ && input_->IsPressButton(XINPUT_GAMEPAD_DPAD_DOWN) && !input_->IsPressButton(XINPUT_GAMEPAD_DPAD_UP))
+			{
+				velocity_.x = 0.0f;
+				isBackMove_ = false;
+			}
+		}
+
+		//移動していない時
+		if (!input_->IsPressButton(XINPUT_GAMEPAD_DPAD_RIGHT) && !input_->IsPressButton(XINPUT_GAMEPAD_DPAD_LEFT))
+		{
+			isGuard_ = false;
+		}
+
+		//移動
+		if (isFrontMove_)
+		{
+			animationIndex = 2;
+
+			UpdateAnimationTime(animationTime, true, 30.0f, animationIndex, model_);
+
+			velocity_ = Normalize(velocity_);
+			velocity_ = Multiply(characterFrontSpeed_, velocity_);
+
+			// 平行移動
+			worldTransform_.translation = Add(worldTransform_.translation, velocity_);
+
+			worldTransform_.UpdateMatrixEuler();
+		}
+		else if (isBackMove_)
+		{
+			animationIndex = 2;
+
+			UpdateAnimationTime(animationTime, true, 60.0f, animationIndex, model_);
+
+			velocity_ = Normalize(velocity_);
+			velocity_ = Multiply(characterBackSpeed_, velocity_);
+
+			// 平行移動
+			worldTransform_.translation = Add(worldTransform_.translation, velocity_);
+
+			worldTransform_.UpdateMatrixEuler();
+		}
+		else
+		{
+			animationIndex = 0;
+
+			UpdateAnimationTime(animationTime, true, 60.0f, animationIndex, model_);
+		}
+
+		//ジャンプ
+		if (input_->GetJoystickState())
+		{
+			if (input_->IsPressButton(XINPUT_GAMEPAD_DPAD_UP) && !input_->IsPressButtonEnter(XINPUT_GAMEPAD_A) && isDown_ == false)
+			{
+				behaviorRequest_ = Behavior::kJump;
+			}
+		}
+	}
+}
+
+void Player::BehaviorAttackInitialize()
+{
+
+}
+
+void Player::BehaviorAttackUpdate()
+{
+
+}
+
+void Player::BehaviorJumpInitialize()
+{
+	worldTransform_.translation.y = 0.0f;
+
+	const float kJumpFirstSpeed_ = 0.6f;
+
+	velocity_.y = kJumpFirstSpeed_;
+}
+
+void Player::BehaviorJumpUpdate()
+{
+	float animationTime = 0.0f;
+	animationIndex = 1;
+
+	UpdateAnimationTime(animationTime, true, 60.0f, animationIndex, model_);
+
+	worldTransform_.translation = Add(worldTransform_.translation, velocity_);
+
+	const float kGravityAcceleration_ = 0.03f;
+
+	Vector3 accelerationVector_ = { 0.0f,-kGravityAcceleration_,0.0f };
+
+	velocity_ = Add(velocity_, accelerationVector_);
+
+	if (worldTransform_.translation.y <= 0.0f)
+	{
+		behaviorRequest_ = Behavior::kRoot;
+		//workAttack_.isJumpAttack = false;
+		worldTransform_.translation.y = 0.0f;
+		animationTime = 0.0f;
+		model_->SetAnimationTime(animationTime);
+	}
+}
+
+void Player::BehaviorThrowInitialize()
+{
+
+}
+
+void Player::BehaviorThrowUpdate()
+{
+
+}
+
+void Player::BehaviorStanInitialize()
+{
+
+}
+
+void Player::BehaviorStanUpdate()
+{
+
+}
+
+void Player::UpdateAnimationTime(float animationTime, bool isLoop, float frameRate, int animationIndex, std::unique_ptr<Model>& modelFighterBody)
+{
+	animationTime = modelFighterBody->GetAnimationTime();
+
+	if (!isDown_)
+	{
+		animationTime += 1.0f / frameRate;
+
+		if (isLoop)
+		{
+			animationTime = std::fmod(animationTime, modelFighterBody->GetAnimation()[animationIndex].duration);
+		}
+	}
+
+	modelFighterBody->SetAnimationTime(animationTime);
+	modelFighterBody->ApplyAnimation(animationIndex);
+	modelFighterBody->Update();
 }
--- Project/Application/GameObject/Character/Player/Player.h
+++ Project/Application/GameObject/Character/Player/Player.h
@@ -12,6 +12,23 @@
 class Player : public IGame3dObject
 {
 public:
+	enum class Behavior
+	{
+		kRoot,
+		kAttack,
+		kJump,
+		kThrow,
+		kStan
+	};
+
+	enum class Direction
+	{
+		Left,
+		Right
+	};
+	
+	~Player();
+
 	void Initialize()override;
 
 	void Update()override;
@@ -25,16 +42,110 @@ public:
 	uint32_t GetAnimationIndex() { return animationIndex; };
 
 private:
+#pragma region プレイヤーの行動関数
+
+	void BehaviorRootInitialize();
+
+	void BehaviorRootUpdate();
+
+	void BehaviorAttackInitialize();
+
+	void BehaviorAttackUpdate();
+
+	void BehaviorJumpInitialize();
+
+	void BehaviorJumpUpdate();
+
+	void BehaviorThrowInitialize();
+
+	void BehaviorThrowUpdate();
+
+	void BehaviorStanInitialize();
+
+	void BehaviorStanUpdate();
+
+#pragma endregion
+
+	void UpdateAnimationTime(float animationTime, bool isLoop, float frameRate, int animationIndex,
+		std::unique_ptr<Model>& modelFighterBody);
+
+private:
+#pragma region インスタンス
+
 	//modelManager
 	ModelManager* modelManager_ = nullptr;
 
 	//input
 	Input* input_ = nullptr;
 
+	//audio
+	Audio* audio_ = nullptr;
+
+#pragma endregion
+
+#pragma region プレイヤーの基本パラメータ
+
+	//behavior
+	Behavior behavior_ = Behavior::kRoot;
+	std::optional<Behavior> behaviorRequest_ = std::nullopt;
+
+	//向いている方向
+	Direction playerDirection = Direction::Right;
+
+	//再生するanimationの番号
+	uint32_t animationIndex = 0;
+
+	//hp
+	float maxHP_ = 10.0f;
+	float HP_ = maxHP_;
+
+	//ガードゲージ
+	float maxGuardGauge_ = 50.0f;
+	float guardGauge_ = 0.0f;
+
+	//必殺技のゲージ
+	float maxFinisherGauge_ = 50.0f;
+	float finisherGauge_ = 50.0f;
+
+#pragma endregion
+
+#pragma region プレイヤーの移動パラメータ
+
+	//移動
+	Vector3 velocity_ = {};
+	float speed_ = 0.3f;
+
+	//足の速さ(向いている方向に移動する場合)
+	float characterFrontSpeed_ = 0.1f;
+
+	//足の速さ(向いている方向とは逆に移動する場合)
+	float characterBackSpeed_ = 0.05f;
+
+#pragma endregion
+
+#pragma region プレイヤーの攻撃パラメータ
+
+#pragma endregion
+
+#pragma region プレイヤーのフラグ
+
+	//当たっているかどうか
+	bool isHit_ = false;
+
+	//ダウンしているかどうか
+	bool isDown_ = false;
+
+	//ガードしているかどうか
+	bool isGuard_ = false;
+
+#pragma endregion
+
+#pragma region パーティクル
+
 	//particle
 	std::unique_ptr<ParticleModel> particleModel_ = nullptr;
 	std::unique_ptr<ParticleSystem> particleSystem_ = nullptr;
 
-	uint32_t animationIndex = 0;
+#pragma endregion
 };
 
