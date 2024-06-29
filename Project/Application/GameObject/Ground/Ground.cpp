aaf6ac77a69e398554b0206f65bfa0dd52e267e7
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-06-29 23:15:07 +0900

    Groundクラスの作成

---
+++ Project/Application/GameObject/Ground/Ground.cpp
@@ -0,0 +1,28 @@
+#include "Ground.h"
+#include <cassert>
+
+void Ground::Initialize()
+{
+	//worldTransformの初期化
+	worldTransform_.Initialize();
+}
+
+void Ground::Update()
+{
+	//worldTransformの更新
+	worldTransform_.UpdateMatrixEuler();
+}
+
+void Ground::Draw(const Camera& camera)
+{
+	model_->Draw(worldTransform_, camera, 0);
+}
+
+void Ground::ImGui()
+{
+	ImGui::Begin("Ground");
+	ImGui::DragFloat3("WTFT", &worldTransform_.translation.x, -14.0f, 14.0f);
+	ImGui::DragFloat3("WTFR", &worldTransform_.rotation.x, 0.0f, 150.0f);
+	ImGui::DragFloat3("WTFS", &worldTransform_.scale.x, 0.0f, 300.0f);
+	ImGui::End();
+}
