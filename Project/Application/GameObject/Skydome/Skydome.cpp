2437b2dc637558aeff63be8916b2663be2c54c0f
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-12-12 03:35:19 +0900

    Skydomeのコメント追加終了

--- Project/Application/GameObject/Skydome/Skydome.cpp
+++ Project/Application/GameObject/Skydome/Skydome.cpp
@@ -10,41 +10,42 @@
 
 void Skydome::Initialize()
 {
-	//inputのinstance
+	//Inputのインスタンスの取得
 	input_ = Input::GetInstance();
 
 	//モデルの生成
 	model_.reset(Model::CreateFromOBJ("resource/skydome", "skydome.obj"));
 
-	//worldTransformの初期化
+	//WorldTransformの初期化
 	worldTransform_.Initialize();
-	worldTransform_.scale = { 250.0f,250.0f,250.0f };
+	worldTransform_.scale = scale_;
 }
 
 void Skydome::Update()
 {
 	//映り込みの設定
-	model_->GetMaterial()->SetEnvironmentCofficient(0.0f);
+	model_->GetMaterial()->SetEnvironmentCofficient(environmentCofficient_);
 
 	//回転
 	worldTransform_.rotation.y += rotationSpeed_.y;
 	worldTransform_.rotation.z += rotationSpeed_.z;
 
-	//worldTransformの更新
+	//WorldTransformの更新
 	worldTransform_.UpdateMatrixEuler();
 }
 
 void Skydome::Draw(const Camera camera)
 {
 	//モデルの描画
-	model_->Draw(worldTransform_, camera, 0);
+	model_->Draw(worldTransform_, camera, animationData_);
 }
 
 void Skydome::ImGui()
 {
+	//ImGui
 	ImGui::Begin("Skydome");
-	ImGui::DragFloat3("WTFT", &worldTransform_.translation.x, -14.0f, 14.0f);
-	ImGui::DragFloat3("WTFR", &worldTransform_.rotation.x, 0.0f, 150.0f);
-	ImGui::DragFloat3("WTFS", &worldTransform_.scale.x, 0.0f, 300.0f);
+	ImGui::DragFloat3("WTFT", &worldTransform_.translation.x, kMinTranslation_, kMaxTranslation_);
+	ImGui::DragFloat3("WTFR", &worldTransform_.rotation.x, kMinRotation_, kMaxRotation_);
+	ImGui::DragFloat3("WTFS", &worldTransform_.scale.x, kMinScale_, kMaxScale_);
 	ImGui::End();
 }
