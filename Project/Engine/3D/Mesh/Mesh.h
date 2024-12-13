91aeede70bf385043dd64761869b048a0c383d04
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2024-12-13 13:09:37 +0900

    SR1途中

--- Project/Engine/3D/Mesh/Mesh.h
+++ Project/Engine/3D/Mesh/Mesh.h
@@ -13,29 +13,39 @@
 class Mesh
 {
 public:
+	/// <summary>初期化</summary>
 	void Initialize(const std::vector<VertexData>& vertices, const std::vector<uint32_t>& indices);
 
+	/// <summary>描画</summary>
 	void Draw();
 
+	/// <summary>グラフィックスコマンドをセット</summary>
 	void SetGraphicsCommand(D3D12_VERTEX_BUFFER_VIEW influenceBufferView);
 
 private:
+	/// <summary>頂点バッファの作成</summary>
 	void CreateVertexBuffer();
 
+	/// <summary>インデックスバッファの作成</summary>
 	void CreateIndexBuffer();
 
 private:
+	//DirectXCoreのポインタ
 	DirectXCore* dxCore_ = nullptr;
 
+	//頂点バッファ
 	Microsoft::WRL::ComPtr<ID3D12Resource> vertexBuffer_ = nullptr;
-
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
-
 	std::vector<VertexData> vertices_{};
 
+	//インデックスバッファ
 	Microsoft::WRL::ComPtr<ID3D12Resource> indexBuffer_ = nullptr;
-
 	D3D12_INDEX_BUFFER_VIEW indexBufferView_{};
-
 	std::vector<uint32_t> indices_{};
+
+	//インスタンスカウント
+	const int  kInstanceCount_ = 1;
+
+	//ビューの数
+	static const int kNumViews_ = 2;
 };
