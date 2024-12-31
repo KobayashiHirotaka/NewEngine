20bb7377c5f0213c2127f9102bc7c8d66f025463
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2025-01-01 02:44:35 +0900

    CharacterDataLoaderクラスを作成

---
+++ Project/Application/GameObject/Character/CharacterDataLoader/CharacterDataLoader.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file CharacterDataLoader.cpp
+ * @brief 各キャラクターのデータをJsonファイルから読み込んで設定する
+ * @author  KOBAYASHI HIROTAKA
+ * @date 2024/12/31
+ */
+
+#include "CharacterDataLoader.h"
---
+++ Project/Application/GameObject/Character/CharacterDataLoader/CharacterDataLoader.h
@@ -0,0 +1,13 @@
+/**
+ * @file CharacterDataLoader.h
+ * @brief 各キャラクターのデータをJsonファイルから読み込んで設定する
+ * @author  KOBAYASHI HIROTAKA
+ * @date 2024/12/31
+ */
+
+#pragma once
+class CharacterDataLoader
+{
+
+};
+
